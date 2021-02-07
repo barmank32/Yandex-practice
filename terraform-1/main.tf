1eb851625b577aaedc90bea7009094a53a627b4f
Denis Palaguta <barmank@yandex.ru>
2021-02-07 21:50:38 +0300

    Terraform 0.13

--- terraform-1/main.tf
+++ terraform-1/main.tf
@@ -4,3 +4,17 @@ provider "yandex" {
   folder_id                = var.folder_id
   zone                     = var.zone
 }
+
+terraform {
+  backend "s3" {
+    endpoint                    = "storage.yandexcloud.net"
+    bucket                      = "tf-state-buckend"
+    region                      = "us-east-1"
+    key                         = "prod.tfstate"
+    shared_credentials_file     = "access.key"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    dynamodb_endpoint           = "https://docapi.serverless.yandexcloud.net/ru-central1/b1g0d9fnkcg0p56pie7g/etn00oae9bl6i6ice58f"
+    dynamodb_table              = "table287"
+  }
+}
